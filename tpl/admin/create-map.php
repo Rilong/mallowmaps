a5845b79478e960ee7489544ddb6f9cac4e19283
Rilong <romgnatyuk@gmail.com>
2018-04-21 15:09:25 +0300

    fix template create map

--- tpl/admin/create-map.php
+++ tpl/admin/create-map.php
@@ -1,16 +1,16 @@
 <div id="mow_admin_map-page" style="display: none;">
     <input id="mowmap_places" type="text" placeholder="Type here your location...">
     <div id="mow_admin_map"></div>
-</div>
-<div class="mowmap-options">
-    <table class="form-table">
-        <tr>
-            <td>Center</td>
-            <td><input type="text" id="mowmap_lng" class="p100" disabled></td>
-            <td><input type="text" id="mowmap_lat" class="p100" disabled></td>
-        </tr>
-        <tr>
-            <td><button id="mowmap_get_center">Get center</button></td>
-        </tr>
-    </table>
+    <div class="mowmap-options">
+        <table class="form-table">
+            <tr>
+                <td>Center</td>
+                <td><input type="text" id="mowmap_lng" class="p100" disabled></td>
+                <td><input type="text" id="mowmap_lat" class="p100" disabled></td>
+            </tr>
+            <tr>
+                <td><button id="mowmap_get_center">Get center</button></td>
+            </tr>
+        </table>
+    </div>
 </div>
